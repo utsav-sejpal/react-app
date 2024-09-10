23e611bc6ba3abe4df493afbe22ab5354afbcbc8
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2024-09-10 19:58:17 +0530

    props validation added

--- src/components/practice/props/SchoolComponent.js
+++ src/components/practice/props/SchoolComponent.js
@@ -10,8 +10,8 @@ export default function SchoolComponent() {
     return (
         <>
             <h1>This is School Component!</h1>
-            {/* <StudentComponent name="Student 1" /> */}
-            <StudentComponent details={details} />
+            <StudentComponent name="Student 1" />
+            {/* <StudentComponent details={details} /> */}
             {/* <StudentComponent subject="React" /> */}
         </>
     )
--- src/components/practice/props/StudentComponent.js
+++ src/components/practice/props/StudentComponent.js
@@ -1,13 +1,20 @@
+import PropTypes from 'prop-types';
+
 function StudentComponent(props) {
     // console.log(props);
     return (
         <>
-            {props.details.name} <br />
-            {props.details.address}
+            {props.name} <br />
             {/* <h3>My Name is {props.name} </h3> */}
             {/* <h3>My Subject is {props.subject}</h3> */}
         </>
     )
 }
+
+StudentComponent.propTypes = {
+    name: PropTypes.string.isRequired,
+};
+
+// StudentComponent.defaultProps = {};
 
 export default StudentComponent;
